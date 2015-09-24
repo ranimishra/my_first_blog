d320c081312522c8b3f00ffe6f2cce9556fe5c4f
ranimishra <sunelirani3110@gmail.com>
2015-09-24 23:37:04 +0530

    did some changess

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,6 @@
-from django import forms
-from .models import Post
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model =	Post
-	fields = ('title', 'text',)
+from	django	import	forms
+from	.models	import	Post
+class	PostForm(forms.ModelForm):
+				class	Meta:
+								model	=	Post
+								fields	=	('title',	'text',)
